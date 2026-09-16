b27cfef359db1389c6aa0d629d72443a3f99b89b
Ruth Santacruz <ruthsantacruzuca@gmail.com>
2026-09-16 13:54:45 -0400

    chore: Spring Boot API REST desde start.spring.io

---
+++ src/main/java/py/edu/uc/lp3/rs/minecraft/MinecraftApplication.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3.rs.minecraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinecraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/py/edu/uc/lp3/rs/minecraft/MinecraftApplicationTests.java
@@ -0,0 +1,13 @@
+package py.edu.uc.lp3.rs.minecraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinecraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
